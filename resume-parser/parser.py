506979e8807f12eb7aef412f6c60faa1e6689968
parul <parulagg27@gmail.com>
2019-04-12 18:13:22 +0530

    Resume file, parser-code added

---
+++ resume-parser/parser.py
@@ -0,0 +1,39 @@
+from io import BytesIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import pprint
+
+def convert_PDFtoText(path, pages=None):
+	if not pages:
+		pagenums = set()
+	else:
+		pagenums = set(pages)
+
+
+	output = BytesIO()
+	resource_manager = PDFResourceManager()
+	codec = 'utf-8'
+
+	converter = TextConverter(resource_manager, output, codec=codec, laparams=LAParams())
+	interpreter = PDFPageInterpreter(resource_manager, converter)
+
+
+	infile = open(path, 'rb')
+	for page in PDFPage.get_pages(infile, pagenums):
+		interpreter.process_page(page)
+
+	infile.close()
+	converter.close()
+	text = output.getvalue()
+	output.close
+	return text
+
+
+def main():
+	pprint.pprint(convert_PDFtoText("../resume-parser/myresume.pdf"))
+
+
+if __name__ == '__main__':
+	main()
